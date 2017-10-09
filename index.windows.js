07f2ed59708f5e0298be520d7ab2214386e4d0bd
Nikola <bigmecoprog@gmail.com>
2017-10-09 12:12:27 +0300

    test native rn win

--- index.windows.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import index from "./Src/screens/index";
-
-AppRegistry.registerComponent('test', () => index);
---
+++ test1/index.windows.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './Src/screens/index.js';
+
+AppRegistry.registerComponent('test', () => App);
